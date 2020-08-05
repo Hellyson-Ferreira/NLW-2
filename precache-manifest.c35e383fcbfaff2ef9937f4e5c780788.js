8fb95c8af035c02ab30dadf055c2f726b2381b61
Hellyson-Ferreira <hellysonferreira.hf1@gmail.com>
2020-08-05 19:29:13 -0300

    Updates

---
+++ precache-manifest.c35e383fcbfaff2ef9937f4e5c780788.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8391ebf573b135f770010b29a65cbd94",
+    "url": "/NLW-2/index.html"
+  },
+  {
+    "revision": "9b5481e927b3ffc18232",
+    "url": "/NLW-2/static/css/main.07201822.chunk.css"
+  },
+  {
+    "revision": "465f574e978ee33d9d4c",
+    "url": "/NLW-2/static/js/2.bb1cc8ca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/NLW-2/static/js/2.bb1cc8ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b5481e927b3ffc18232",
+    "url": "/NLW-2/static/js/main.e9b04c89.chunk.js"
+  },
+  {
+    "revision": "980cfb8a23906ea3c4e9",
+    "url": "/NLW-2/static/js/runtime-main.fa2f4874.js"
+  },
+  {
+    "revision": "dc8c47fa945838a208074157d997436d",
+    "url": "/NLW-2/static/media/back.dc8c47fa.svg"
+  },
+  {
+    "revision": "156102ee75a899123483211bbf4a8fbd",
+    "url": "/NLW-2/static/media/give-classes.156102ee.svg"
+  },
+  {
+    "revision": "211f2bb00860f91804cfb2354e17b255",
+    "url": "/NLW-2/static/media/landing.211f2bb0.svg"
+  },
+  {
+    "revision": "86fef799b0726c66fb7a9d710e2d922c",
+    "url": "/NLW-2/static/media/logo.86fef799.svg"
+  },
+  {
+    "revision": "bfdb3ebe513b5b2411a5014d6aad3abe",
+    "url": "/NLW-2/static/media/purple-heart.bfdb3ebe.svg"
+  },
+  {
+    "revision": "e7a7e1b4e016245fa97e6f42aee4bcaa",
+    "url": "/NLW-2/static/media/study.e7a7e1b4.svg"
+  },
+  {
+    "revision": "6dfaa2f94fc54e53666a511b779f90b4",
+    "url": "/NLW-2/static/media/warning.6dfaa2f9.svg"
+  },
+  {
+    "revision": "45c6e6ecebde5ee441644d41cc132ce4",
+    "url": "/NLW-2/static/media/whatsapp.45c6e6ec.svg"
+  }
+]);
